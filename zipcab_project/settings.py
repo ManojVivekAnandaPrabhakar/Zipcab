1f8d9e9117b2cd9be2c9246c606e0a11b1413a9b
ManojVivekAnandaPrabhakar <manojvivekanandaprabhakar@gmail.com>
2025-07-02 22:30:16 +0530

    venv

--- zipcab_project/settings.py
+++ zipcab_project/settings.py
@@ -95,7 +95,10 @@ DATABASES = {
 }
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
