b15e6ee5135b48282cd521997318702465f16c5a
ManojVivekAnandaPrabhakar <manojvivekanandaprabhakar@gmail.com>
2025-07-04 12:33:20 +0530

    db for render

--- zipcab_project/settings.py
+++ zipcab_project/settings.py
@@ -62,17 +62,15 @@ TEMPLATES = [
 ]
 
 # ✅ Use PostgreSQL from Render (via DATABASE_URL)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 # Password validators
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
